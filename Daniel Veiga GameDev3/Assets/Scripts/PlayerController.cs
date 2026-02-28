1f7191bcbc6b4057e1457c76b1d89f24e1ea7398
alunosg <alunosg.altodalapa@gmail.com>
2026-02-28 15:41:22 -0300

    Alpha 1.0.0
    Start

---
+++ Daniel Veiga GameDev3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rig;
+    public Collider col;
+    public float speed = 10;
+    public float jumpForce = 10;
+    public LayerMask floorlayer;
+
+    private Vector2 moveInput;
+
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rig.linearVelocity = new(moveInput.x * speed, rig.linearVelocity.y, moveInput.y * speed);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Vector3 p1 = new(col.bounds.max.x, col.bounds.max.y, col.bounds.max.z);
+            Vector3 p2 = new(col.bounds.min.x, col.bounds.max.y, col.bounds.min.z);
+            Vector3 p3 = new(col.bounds.min.x, col.bounds.max.y, col.bounds.max.z);
+            Vector3 p4 = new(col.bounds.max.x, col.bounds.max.y, col.bounds.min.z);
+            Vector3 p5 = new(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z);
+            if (Physics.Raycast(p1, Vector3.down, col.bounds.size.y * 1.01f, floorlayer) ||
+                Physics.Raycast(p2, Vector3.down, col.bounds.size.y * 1.01f, floorlayer) ||
+                Physics.Raycast(p3, Vector3.down, col.bounds.size.y * 1.01f, floorlayer) ||
+                Physics.Raycast(p4, Vector3.down, col.bounds.size.y * 1.01f, floorlayer) ||
+                Physics.Raycast(p5, Vector3.down, col.bounds.size.y * 1.01f, floorlayer))
+            {
+                rig.linearVelocity = new(rig.linearVelocity.x, jumpForce, rig.linearVelocity.z);
+            }
+        }
+    }
+}
