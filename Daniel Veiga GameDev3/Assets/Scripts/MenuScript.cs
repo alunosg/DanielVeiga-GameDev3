aa36bc6bfa8a213c113f19b63d3a382a6c4cbb9b
alunosg <alunosg.altodalapa@gmail.com>
2026-04-18 15:39:47 -0300

    abobara

---
+++ Daniel Veiga GameDev3/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript
+{
+    [SerializeField] private string nomeDoLevel;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painel;
+    
+
+
+
+
+
+
+
+
+}
